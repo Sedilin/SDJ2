01e8d70e1a46298bac8013ff2fa67b37bc5416bd
Sedilin <sramkowskilukasz@gmail.com>
2022-09-01 09:18:45 +0200

    Bear and Poking man

---
+++ src/Session2/Bear/Bear.java
@@ -0,0 +1,19 @@
+package Session2.Bear;
+
+public class Bear implements Runnable{
+
+    @Override
+    public void run() {
+        System.out.println("Bear is feeling tired");
+        System.out.println("Bear goes to sleep");
+
+        try{
+            Thread.sleep(10000);
+        } catch (InterruptedException e)
+        {
+            System.out.println("Angry bear grrrr");
+        }
+
+
+    }
+}
---
+++ src/Session2/Bear/PokingMan.java
@@ -0,0 +1,26 @@
+package Session2.Bear;
+
+public class PokingMan implements Runnable{
+
+    private Thread bearThread;
+
+    public PokingMan(Thread bearThread)
+    {
+        this.bearThread = bearThread;
+    }
+
+    @Override
+    public void run() {
+        System.out.println("Man is walking");
+        System.out.println("Man sees a bear, wishes to poke a bear");
+
+        try {
+            Thread.sleep(5000);
+        } catch (InterruptedException e) {
+            System.out.println("Lol");
+        }
+
+        bearThread.interrupt();
+        System.out.println("RIP");
+    }
+}
